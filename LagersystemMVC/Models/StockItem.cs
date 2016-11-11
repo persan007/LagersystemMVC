49ef0b6b1888f283915541ceb4a7d05a35124052
persan007 <per-ake.sandstrom@hotmail.se>
2016-11-11 08:48:37 +0100

    https://github.com/persan007/LagersystemMVC.git

---
+++ LagersystemMVC/Models/StockItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+
+
+namespace LagersystemMVC.Models
+{
+    public class StockItem
+    {
+        [Key]                               //Gör ItemID till en primary key
+        public int ItemID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Shelf { get; set; }
+        public string Description { get; set; }
+    }
+}
